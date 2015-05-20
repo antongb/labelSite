998ceaabe076e78bab03c588f842c70e0ce24c48
Anton Shain <shainanton@gmail.com>
2015-05-19 20:07:01 -0700

    added template

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,9 @@ Rails.application.configure do
     :authentication => :plain,
   }
 
+  #Set up queueing backend.
+  config.active_job.queue_adapter = :delayed_job
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
